8921abae82fd878ae8fd766d51207ffd59578eab
Eduardo Salcedo <86426063+Eds-salcedo@users.noreply.github.com>
2024-12-12 18:30:18 +0100

    Create 01-functions-intro.py
    A quick start to know what DEF is and what it does.

---
+++ code/01-functions-intro.py
@@ -0,0 +1,17 @@
+"""
+LET'S REWIND WHAT FUNCTIONS DO WE KNOW SO FAR AND LEARN WHAT WE CAN DO
+"""
+
+# We've used, for example, PRINT() which will perform a specific action with the value that we enter.
+print("Hola Mundo")
+print("DONE")
+
+# We can create our own functions with def: def FunctionName(ValueToBeIntroduced): WhatWeWantToDo
+
+
+def hola():
+    print("Hola Mundo!")
+    print("Ultimate Python")
+
+
+hola()
