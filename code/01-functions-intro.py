f851eab6c5db357e9e5cc1effcc7645d6818aa8e
Eduardo Salcedo <86426063+Eds-salcedo@users.noreply.github.com>
2024-12-12 18:52:13 +0100

    Update 01-functions-intro.py
    Addition of of the explaination of what parameters and arguments are, as well as the explaination about how functions work with several parameters.

--- code/01-functions-intro.py
+++ code/01-functions-intro.py
@@ -12,6 +12,35 @@ print("DONE")
 def hola():
     print("Hola Mundo!")
     print("Ultimate Python")
+    print("Welcome Eduardo")
 
 
 hola()
+
+# Parameters
+
+
+def hello(name):  # Here we introduced a value within the parentheses.
+    print("Hello World!")
+    print(f"Welcome {name}")
+
+
+# Adding that previous value within the parentheses turned the function into an OBLIGATORY-variable function, so the function will always have to be called along with a variable. If not, then the call of the function won't work.
+# hello() -- Error
+
+hello("eduardo")
+hello("Xyzzz")
+
+# IMPORTANT: The value that we put within the def value parentheses is called PARAMETER.
+# When you call the function and introduce a custom value within a def function parentheses, it becomes an ARGUMENT.
+
+# Several parameters: When you create a function, you can configue it so it mandatorily receives a certain number of desired arguments.
+
+
+def bonjour(nom, age):
+    print(f"Ton nom est {nom} et ton âge est {age}")
+
+
+# bonjour("eduardo") -- Error
+
+bonjour("eduardo", 29)
