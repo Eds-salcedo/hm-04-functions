8beccadc2a189c1d20577ce6ca36bbca8987cf81
Eduardo Salcedo <86426063+Eds-salcedo@users.noreply.github.com>
2024-12-14 11:53:26 +0100

    Update 01-functions-intro.py
    Addition of the Called Arguments section, in which we learned how to properly use the identification of every introduced argument with its corresponding parameter whenever we don't remember the exact order in which we should introduce the mentioned arguments of a DEF function.

--- code/01-functions-intro.py
+++ code/01-functions-intro.py
@@ -6,7 +6,7 @@ LET'S REWIND WHAT FUNCTIONS DO WE KNOW SO FAR AND LEARN WHAT WE CAN DO
 print("Hola Mundo")
 print("DONE")
 
-# We can create our own functions with def: def FunctionName(ValueToBeIntroduced): WhatWeWantToDo
+# 1) We can create our own functions with def: def FunctionName(ValueToBeIntroduced): WhatWeWantToDo
 
 
 def hola():
@@ -17,7 +17,7 @@ def hola():
 
 hola()
 
-# Parameters
+# 2) Functions with a parameter:
 
 
 def hello(name):  # Here we introduced a value within the parentheses.
@@ -31,16 +31,42 @@ def hello(name):  # Here we introduced a value within the parentheses.
 hello("eduardo")
 hello("Xyzzz")
 
-# IMPORTANT: The value that we put within the def value parentheses is called PARAMETER.
+# Important: The value that we put within the def value parentheses is called PARAMETER.
 # When you call the function and introduce a custom value within a def function parentheses, it becomes an ARGUMENT.
 
-# Several parameters: When you create a function, you can configue it so it mandatorily receives a certain number of desired arguments.
+# 3) Several parameters:
+# When you create a function, you can configue it so it mandatorily receives a certain number of desired arguments.
 
 
 def bonjour(nom, age):
     print(f"Ton nom est {nom} et ton âge est {age}")
 
 
+def Hi(name, last_name, city):
+    print(name, last_name, city)
+
 # bonjour("eduardo") -- Error
 
+
 bonjour("eduardo", 29)
+
+# 4) Optional parameters: In case that there is a parameter that you do not require to be filled mandatorily by all users, you can auto-fill it with a default value in case it remains empty
+
+
+def olá(nome, edade, cidade="nenhuma"):
+    print(f"O seu nome é {nome}, a sua edade é {
+          edade} e sua cidade é {cidade}")
+
+
+# If you add the CIDADE value, the function will take it. Otherwise, instead of generating an error, it will return the default value that you introduced in your function.
+olá("Eduardo", 29, "Madrid")
+olá("Jose", 30)
+
+# 5) Called arguments: In case that we need to provide certain data required by the code, but we don't know the specific order in how we should enter it, we can enter the data specifying the corresponding name of the field we are providing the data for.
+# In this case, if we do it for one, we must do it for all, so we'll have to specify the exact parameter name for which we are introducing the argument.
+# Otherwise, if you name just 1 parameter with its corresponding argument, but you leave the other arguments without its structure, Pyhon will consider it as an error.
+
+# Hi(last_name="Salcedo", eduardo) - - Error
+
+Hi(last_name="Salcedo", city="Madrid", name="Eduardo")
+
